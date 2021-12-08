b4416d7e7d9ee4c21c8a99557ca08ade988aa99c
Carlos Herrera <studio3dcad@gmail.com>
2021-12-08 20:56:26 -0300

    clase 39 eliminar novedades

--- models/novedadesModels.js
+++ models/novedadesModels.js
@@ -6,4 +6,20 @@ async function getNovedades(){
     return rows;
 }
 
-module.exports={ getNovedades}
+async function insertNovedad(obj){
+    try{
+        var query = "insert into novedades set ?";
+        var rows = await pool.query(query,[obj]);
+        return rows;
+    }catch (error){
+        console.log(error);
+        throw error;
+    }
+}
+
+async function deleteNovedadByID(id){
+    var query = 'delete from novedades where id=?'
+    var rows = await pool.query(query,[id]);
+    return rows;
+}
+module.exports={ getNovedades, insertNovedad, deleteNovedadByID}
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -21,4 +21,40 @@ router.get('/agregar', function (req, res, next) {
         layout: 'admin/layout'
     });
 });
+/*procesa o da funcionamiento al boton guardar*/
+
+router.post('/agregar', async (req, res, next) => {
+    try {
+        console.log(req.body);
+
+        if(req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != ""){
+            await novedadesModel.insertNovedad(req.body);
+            res.redirect('/admin/novedades');
+        }else{
+            res.render('admin/agregar',{
+                layout:'admin/layout',
+                error:true,
+                message: 'Todos los campos son requeridos'
+            })
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se cargo la novedad'
+        })
+    }
+})
+
+/*funcionamiento de eliminar*/
+
+router.get('/eliminar/:id',async(req,res,next)=>{
+    var id = req.params.id; //captura el id 
+
+    await novedadesModel.deleteNovedadByID(id);
+    res.redirect('/admin/novedades');
+}); 
+
 module.exports = router;
